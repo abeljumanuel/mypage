71dd332c6a924790d59117998206d98b05c523f5
Juan Manuel Rojas Gonzalez <jrojasgo@imi.mil.co>
2022-02-26 17:51:40 -0500

    Adjusting Everything !!!

--- js/behavior.js
+++ js/behavior.js
@@ -1,11 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    var thumbnailElement = document.getElementById("smart_thumbnail");
-    thumbnailElement.addEventListener("click",function(){
+    var thumbnailElement_1 = document.getElementById("smart_thumbnail_love");
+    thumbnailElement_1.addEventListener("click",function(){
 
-       if (thumbnailElement.className == "") {
-        thumbnailElement.className = "small";
+       if (thumbnailElement_1.className == "") {
+        thumbnailElement_1.className = "small";
        }else{
-        thumbnailElement.className = "";
+        thumbnailElement_1.className = "";
+       }
+
+    });
+    var thumbnailElement_2 = document.getElementById("smart_thumbnail_book");
+    thumbnailElement_2.addEventListener("click",function(){
+
+       if (thumbnailElement_2.className == "") {
+        thumbnailElement_2.className = "small";
+       }else{
+        thumbnailElement_2.className = "";
        }
 
     });
